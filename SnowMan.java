306921cb9a285b2bb5d626eae42967aa3673d9a1
Sang Nguyen <shn2020@email.vccs.edu>
2016-04-04 14:46:20 -0400

    first commit

---
+++ SnowMan.java
@@ -0,0 +1,13 @@
+import javax.swing.JApplet;
+import java.awt.Graphics;
+
+public class SnowMan extends JApplet {
+	public void paint(Graphics canvas) {
+		canvas.drawOval(100, 25, 200, 200);
+		canvas.fillOval(155, 100, 10, 20);
+		canvas.fillOval(230, 100, 10, 20);
+		canvas.drawArc(150, 160, 100, 50, 180, 180);
+		canvas.drawOval(75, 225, 250, 250);
+		canvas.drawOval(50, 475, 300, 300);
+	}
+}
